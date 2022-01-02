2616de9c24d6a29d0ea225a065ec2a5776ac871c
marisasaunders <marisaksaunders@gmail.com>
2022-01-02 15:40:12 -0500

    contact sheet again

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-const gallery = document.getElementById('gallery');
-
-
-const imageIndexes = [1,2,3];
-
-imageIndexes.forEach((i) => {
-    const photos = document.createElement('img');
-    photos.src = `/photos/album_${i}.jpg`;
-    photos.alt = `KIMIKO album ${i}`;
-    photos.classList.add('galleryImg');
+function validate(){
+    var name = document.getElementById("name").value;
+    var subject = document.getElementById("subject").value;
+    var phone = document.getElementById("phone").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    var error_message = document.getElementById("error_message");
     
-    })
-
-    gallery.appendChild(photos);
-});
+    error_message.style.padding = "10px";
+    
+    var text;
+    if(name.length < 5){
+      text = "Name must be longer than 5 charaters";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(subject.length < 10){
+      text = "Subject must be longer than 10 characters";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(isNaN(phone) || phone.length != 10){
+      text = "Please Enter valid Phone Number";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(email.indexOf("@") == -1 || email.length < 6){
+      text = "Please Enter valid Email";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(message.length <= 140){
+      text = "Please Enter More Than 140 Characters";
+      error_message.innerHTML = text;
+      return false;
+    }
+    alert("Form Submitted Successfully!");
+    return true;
+  }
